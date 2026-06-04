723be2d9e1bf5591a443050b7351a5ea71874d24
VR Admin <admin@vrspace.local>
2026-06-04 08:55:15 +0800

    style: 营销效果报表 KPI 卡片改为两排展示
    - xl 屏幕下从 9 列改为 5 列，9 张卡片自动分为 5+4 两排
    - 缓解卡片过度拥挤的问题

--- app/src/pages/CouponEffects.tsx
+++ app/src/pages/CouponEffects.tsx
@@ -260,7 +260,7 @@ export default function CouponEffects() {
         </div>
 
         {/* KPI Cards */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-9 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4">
           {kpiCards.map((kpi, i) => (
             <motion.div
               key={kpi.label}
